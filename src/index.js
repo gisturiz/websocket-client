229c90a676ae37016a765745b6f963863201244c
Gustavo Isturiz <isturizgustavo@gmail.com>
2021-03-31 17:33:41 -0400

    messages being received in server

--- src/index.js
+++ src/index.js
@@ -6,15 +6,27 @@ const client = new W3CWebsocket('ws://127.0.0.1:8000');
 
 export default function App() {
 
+    const onButtonClicked = (value) => {
+        client.send(JSON.stringify({
+            type: "message",
+            msg: value,
+        }));
+    };
+
     useEffect(() => {
         client.onopen = () => {
             console.log('WebSocket Client Connected');
-        }
-    }, [])
+        };
+
+        client.onmessage = (message) => {
+            const dataFromServer = JSON.parse(message.data);
+            console.log('got reply', dataFromServer);
+        };
+    });
 
     return (
         <div>
-            Testing App!
+            <button onClick={() => onButtonClicked("Hello")}>Send Message</button>
         </div>
     )
 }
